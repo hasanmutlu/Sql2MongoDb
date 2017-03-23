4a0183275e686565381c5f638dca9746feac7f06
Hasan MUTLU <hasanmutlu9@gmail.com>
2017-03-23 08:36:12 +0300

    Post Process and Filter added

---
+++ Sql2MongoDb/MongoConverterOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql2MongoDb
+{
+    public class MongoConverterOptions
+    {
+        private string _mongoCollectionName;
+        public string SqlTableName { get; set; }
+        public string MongoDataBaseName {
+            get { return _mongoCollectionName; }
+            set
+            {
+                _mongoCollectionName = string.IsNullOrEmpty(value) ? value.Split('.').Last() : value;
+            }
+        }
+        public string MongoCollectionName { get; set; }
+        public string MongoCollectionIndex { get; set; }
+        public Func<object, object> PostProcess { get; set; }
+        public Func<object, bool> FilterProcess { get; set; }
+
+
+    }
+}
