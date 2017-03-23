b8dde86844af4b28820c3eaef83432c375708330
Hasan MUTLU <hasanmutlu9@gmail.com>
2017-03-23 08:37:31 +0300

    Post Process and Filter

--- Sql2MongoDb/Sql2MongoDbConverter.cs
+++ Sql2MongoDb/Sql2MongoDbConverter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Dynamic;
-using System.Linq;
 using MongoDB.Driver;
 
 namespace Sql2MongoDb
@@ -13,32 +12,14 @@ namespace Sql2MongoDb
         private static SqlConnection _sqlConnection;
         private static MongoClient _mongoClient;
         public event Action<Exception> OnError;
+
         public Sql2MongoDbConverter(SqlConnection sqlConnection, MongoClient mongoClient)
         {
             _sqlConnection = sqlConnection;
             _mongoClient = mongoClient;
         }
 
-        public void ConvertTable<T>(string sqlTableName,string mongoDataBaseName,string mongoCollectionName=null) where T:new()
-        {
-            try
-            {
-                mongoCollectionName = string.IsNullOrEmpty(mongoCollectionName)? sqlTableName.Split('.').Last(): mongoCollectionName;
-                _sqlConnection.Open();
-                var db = _mongoClient.GetDatabase(mongoDataBaseName);
-                var collection = db.GetCollection<T>(mongoCollectionName);
-                ConvertTable(collection,sqlTableName);
-                _sqlConnection.Close();
-                _sqlConnection.Dispose();
-                
-            }
-            catch (Exception ex)
-            {
-                NotifyError(ex);
-            }
-        }
-
-        public void ConvertDataBase(string sqlDatabaseName,string mongoDatabaseName=null)
+        public void ConvertDataBase(string sqlDatabaseName, string mongoDatabaseName = null)
         {
             try
             {
@@ -49,7 +30,7 @@ namespace Sql2MongoDb
                 foreach (var table in tableList)
                 {
                     var collection = db.GetCollection<dynamic>(table.Key);
-                    ConvertTable(collection, table.Value);
+                    ConvertTable(collection,new MongoConverterOptions {SqlTableName = table.Value});
                 }
                 _sqlConnection.Close();
                 _sqlConnection.Dispose();
@@ -60,25 +41,18 @@ namespace Sql2MongoDb
                 NotifyError(ex);
             }
         }
-
-        private void ConvertTable<T>(IMongoCollection<T> collection, string sqlTableName) where T:new()
+        private Dictionary<string, int> GetTableFields(IDataRecord dataReader)
         {
-                var selectCommand = new SqlCommand($"SELECT * FROM {sqlTableName};", _sqlConnection);
-                var dataReader = selectCommand.ExecuteReader();
-                if (!dataReader.HasRows)
-                {
-                    return;
-                }
-                var tableFields = GetTableFields(dataReader);
-                while (dataReader.Read())
-                {
-                    var row = GetRowData<T>(dataReader, tableFields);
-                    collection.InsertOne(row);
-                }
-                selectCommand.Dispose();
-                dataReader.Dispose();
+            var result = new Dictionary<string, int>();
+            for (var i = 0; i < dataReader.FieldCount; i++)
+            {
+                var fieldName = dataReader.GetName(i);
+                result.Add(fieldName, i);
+            }
+            return result;
         }
-        private Dictionary<string,string> GetTablesOfDatabase(string databaseName)
+
+        private Dictionary<string, string> GetTablesOfDatabase(string databaseName)
         {
             var result = new Dictionary<string, string>();
             var cmd = new SqlCommand($"SELECT TABLE_SCHEMA,TABLE_NAME FROM {databaseName}.INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'", _sqlConnection);
@@ -88,33 +62,72 @@ namespace Sql2MongoDb
                 while (reader.Read())
                 {
                     var tableName = $"{databaseName}.{reader.GetString(0)}.{reader.GetString(1)}";
-                    result.Add(reader.GetString(1),tableName);
+                    result.Add(reader.GetString(1), tableName);
                 }
             }
             return result;
         }
 
-
-        private T GetRowData<T>(IDataRecord dataReader, Dictionary<string, int> tableFields) where T:new()
+        public void ConvertTable(MongoConverterOptions options)
         {
-            dynamic result = new ExpandoObject();
+            try
+            {
+                _sqlConnection.Open();
+                var db = _mongoClient.GetDatabase(options.MongoDataBaseName);
+                var collection = db.GetCollection<dynamic>(options.MongoCollectionName);
+                if (!string.IsNullOrEmpty(options.MongoCollectionIndex))
+                {
+                    collection.Indexes.CreateOne(options.MongoCollectionIndex);
+                }
+                ConvertTable(collection,options);
+                _sqlConnection.Close();
+                _sqlConnection.Dispose();
+                
+            }
+            catch (Exception ex)
+            {
+                NotifyError(ex);
+            }
+        }
+
+        private void ConvertTable(IMongoCollection<dynamic> collection, MongoConverterOptions options)
+        {
+                var selectCommand = new SqlCommand($"SELECT * FROM {options.SqlTableName};", _sqlConnection);
+                var dataReader = selectCommand.ExecuteReader();
+                if (!dataReader.HasRows)
+                {
+                    return;
+                }
+                var tableFields = GetTableFields(dataReader);
+                while (dataReader.Read())
+                {
+                    var row = GetRowData(dataReader, tableFields);
+                    if (options.FilterProcess!=null)
+                    {
+                        if (!options.FilterProcess(row))
+                        {
+                            continue;
+                        }
+                    }
+                    if (options.PostProcess != null)
+                    {
+                        row = options.PostProcess(row);
+                    }
+                    collection.InsertOne(row);
+                }
+                selectCommand.Dispose();
+                dataReader.Dispose();
+        }
+
+        private dynamic GetRowData(IDataRecord dataReader, Dictionary<string, int> tableFields)
+        {
+            var result = new ExpandoObject();
             var resultContainer = ((IDictionary<string, object>)result);
             foreach (var item in tableFields)
             {
                 var value = dataReader.GetValue(item.Value);
                 value = value is DBNull ? null : value;
                 resultContainer.Add(item.Key, value);
-            }
-            return (T)result;
-        }
-
-        private Dictionary<string, int> GetTableFields(IDataRecord dataReader)
-        {
-            var result = new Dictionary<string, int>();
-            for (var i = 0; i < dataReader.FieldCount; i++)
-            {
-                var fieldName = dataReader.GetName(i);
-                result.Add(fieldName, i);
             }
             return result;
         }
